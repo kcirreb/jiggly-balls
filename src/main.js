bb4b33a925cbeca9e21203015d38511789f524ae
kcirreb <koberrick11@gmail.com>
2025-02-01 01:33:14 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import * as THREE from "three";
+import RAPIER from "https://cdn.skypack.dev/@dimforge/rapier3d-compat";
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.z = 5;
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+function handleWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+window.addEventListener("resize", handleWindowResize, false);
